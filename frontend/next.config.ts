c7c48a9eec0c25e2b2c1eb5c702d1c3d1f3cfc13
Tuyet Mieu <kiennc.23ba14158@usth.edu.vn>
2026-08-13 09:46:55 +0700

    ...

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
-  outputFileTracingRoot: path.join(__dirname),
   turbopack: {
     root: path.join(__dirname),
   },
